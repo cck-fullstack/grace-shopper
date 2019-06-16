2d504dadfa0c578e0742552ef6087b57d87244b3
William Golden V <wtgolden5@gmail.com>
2019-06-16 18:16:29 -0400

    style: updated part of the navbar

--- client/components/footer.js
+++ client/components/footer.js
@@ -6,22 +6,32 @@ const Footer = () => {
       <div className="container">
         <div className="row">
           <div className="col l6 s12">
-            <h5 className="white-text">Footer Content</h5>
-            <p className="grey-text text-lighten-4">
-              You can use rows and columns here to organize your footer content.
-            </p>
+            <h5 className="white-text">Created By:</h5>
+            <ul className="grey-text text-lighten-4">
+              <li>Alex Mok</li>
+              <li>Will Golden</li>
+              <li>Stan So</li>
+              <li>Eric Kreiter</li>
+            </ul>
           </div>
           <div className="col l4 offset-l2 s12">
             <h5 className="white-text">Links</h5>
-            <a className="social-icon" />
+            <ul className="grey-text text-lighten-4">
+              <a className="waves-effect waves-light social-icon btn linkedin">
+                <i className="fa fa-linkedin" />
+              </a>
+              <li>Will Golden</li>
+              <li>Stan So</li>
+              <li>Eric Kreiter</li>
+            </ul>
           </div>
         </div>
       </div>
       <div className="footer-copyright">
         <div className="container">
-          © 2019 Alex and the Mokkasins
+          © 2019 Particle Industries
           <a className="grey-text text-lighten-4 right" href="#!">
-            Grace Shopper Project
+            Aggregating leading edge web readiness in the 21st century.
           </a>
         </div>
       </div>
